159704febdf0af6db1acf5414e2a7da2a0afb8f1
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 19:11:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ db = sqlalchemy.create_engine(
         database="spiderIndexer",
         query={"unix_socket": "/cloudsql/cloudcomputing3032:us-central1:spiderindexer"},
     ),
+    pool_size=5,
+    max_overflow=2,
+    pool_timeout=30,  # 30 seconds
+    pool_recycle=1800,  # 30 minutes
 )
 
 conn = engine.connect()
