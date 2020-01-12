db0573c86336839ca91957ec06dfdfceb411b1a8
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 14:32:24 +0000

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -6,15 +6,13 @@ DROP TABLE IF EXISTS employees
 
 CREATE TABLE employees (
     emp_no      INT             NOT NULL,
-    birth_date  DATE            NOT NULL,
     first_name  VARCHAR(14)     NOT NULL,
     last_name   VARCHAR(16)     NOT NULL,
     gender      ENUM ('M','F')  NOT NULL,    
-    hire_date   DATE            NOT NULL,
     PRIMARY KEY (emp_no)
 );
 
-INSERT INTO `employees` VALUES (10001,'1953-09-02','Georgi','Facello','M','1986-06-26'),
-(10002,'1964-06-02','Bezalel','Simmel','F','1985-11-21'),
-(10050,'1958-05-21','Yinghua','Dredge','M','1990-12-25'),
-(10113,'1963-11-13','Jaewon','Syrzycki','M','1989-12-24');
+INSERT INTO `employees` VALUES (10001,'Georgi','Facello','M'),
+(10002,'Bezalel','Simmel','F'),
+(10050,'Yinghua','Dredge','M'),
+(10113,'Jaewon','Syrzycki','M');
