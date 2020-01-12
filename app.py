21ce835d3c5786787e460aec152c223ce01a3377
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 18:04:51 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+import mysql.connector
+
+with open('article.json') as json_data: d=json.load(json_data)
+
+  db_connection = mysql.connector.connect(
+  socketPath="/cloudsql/cloudcomputing3032:us-central1:spiderIndexer",
+  user="root",
+  passwd="password",
+  database="spiderIndexer"
+  )
+db_cursor = db_connection.cursor()
+
+for page in d:
+    sqlInputQuery = 'INSERT INTO webpages VALUES (' + page['URL'] + ', ' + page['webContent'] + ');'
+    db_cursor.execute(sqlInputQuery)
+
+db_connection.commit()
