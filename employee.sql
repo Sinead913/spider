957f816793d8f3da305200e62cc690e7ea059a28
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 14:28:41 +0000

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -13,3 +13,8 @@ CREATE TABLE employees (
     hire_date   DATE            NOT NULL,
     PRIMARY KEY (emp_no)
 );
+
+INSERT INTO `employees` VALUES (10001,'1953-09-02','Georgi','Facello','M','1986-06-26'),
+(10002,'1964-06-02','Bezalel','Simmel','F','1985-11-21'),
+(10050,'1958-05-21','Yinghua','Dredge','M','1990-12-25'),
+(10113,'1963-11-13','Jaewon','Syrzycki','M','1989-12-24');
