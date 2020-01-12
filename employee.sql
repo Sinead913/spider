0630b2c48f0896004e4029329455e886b82a1e37
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 18:05:50 +0000

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -2,14 +2,9 @@ DROP DATABASE IF EXISTS spiderIndexer;
 CREATE DATABASE IF NOT EXISTS spiderIndexer;
 USE spiderIndexer;
 
-SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
-
 DROP TABLE IF EXISTS webpages;
 
 CREATE TABLE webpages (
     URL  VARCHAR(14)     NOT NULL,
     webContent   VARCHAR(16)     NOT NULL
 );
-
-SELECT 'LOADING employees' as 'INFO';
-source article.csv ;
