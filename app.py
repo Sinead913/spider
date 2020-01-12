5f4bb366a85b84aff8a5b8107bd9f26a0b7fb082
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 18:42:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,25 @@ import json
 import mysql.connector
 
 with open('article.json') as json_data: d=json.load(json_data)
+  
+  
+db = sqlalchemy.create_engine(
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        user="root",
+        password="password",
+        database="spiderIndexer"
+        query={"unix_socket": "/cloudsql/cloudcomputing3032:us-central1:spiderindexer",
+    ),
+)
 
-db_connection = mysql.connector.connect(
-#   socketPath="/cloudsql/cloudcomputing3032:us-central1:spiderindexer",
-  user="root",
-  passwd="password",
-  database="spiderIndexer"
-  )
-db_cursor = db_connection.cursor()
+conn.execute(students.insert(), [
+   {'name':'Rajiv', 'lastname' : 'Khanna'},
+   {'name':'Komal','lastname' : 'Bhandari'},
+   {'name':'Abdul','lastname' : 'Sattar'},
+   {'name':'Priya','lastname' : 'Rajhans'},
+])
 
 for page in d:
-  sqlInputQuery = 'INSERT INTO webpages VALUES (' + page['URL'] + ', ' + page['webContent'] + ');'
-  db_cursor.execute(sqlInputQuery)
-
-db_connection.commit()
+  conn.execute(students.insert(), page)
+               
