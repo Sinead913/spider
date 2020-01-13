01b413e914907479ddde95fd061c8a4156125f60
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-13 10:41:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import json
 import mysql.connector
 import sqlalchemy
+import pymysql
 
 with open('article.json') as json_data: d=json.load(json_data)
   
   
 db = sqlalchemy.create_engine(
-  "mysql+pymysql://root:password@/spiderIndexer?unix_socket=/cloudsql/cloudcomputing3032:us-central1:spiderindexer",
-#     sqlalchemy.engine.url.URL(
-#         drivername="mysql+pymysql",
-#         user="root",
-#         password="password",
-#         database="spiderIndexer",
-#         query={"unix_socket": "/cloudsql/cloudcomputing3032:us-central1:spiderindexer"},
-#     ),
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        user="root",
+        password="password",
+        database="spiderIndexer",
+        query={"unix_socket": "/cloudsql/cloudcomputing3032:us-central1:spiderindexer"},
+    ),
     pool_size=5,
     max_overflow=2,
     pool_timeout=30,  # 30 seconds
